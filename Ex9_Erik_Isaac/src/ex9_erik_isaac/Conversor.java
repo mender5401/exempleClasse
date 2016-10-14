2cd92d99dc8470b822ca16ed03dce5ade2aa2a3f
Erick <>
2016-10-14 18:14:10 +0200

    Version : 1.1 comint

---
+++ Ex9_Erik_Isaac/src/ex9_erik_isaac/Conversor.java
@@ -0,0 +1,26 @@
+package ex9_erik_isaac;
+
+
+/**
+ * Escriu un formulari que converteixi nombres inferiors a 5000 a xifres romanes.
+ * El valor dels diferents nombres romans és el següent:
+ * I=1, V=5, X=10, L=50, C=100, D=500, M=1000.
+ * @author alumne
+ */
+public class Conversor {
+    
+    public Conversor(){
+    
+       
+    }
+    
+    public String numberToromanic(int number){
+      String romanic ="";
+      int miles;
+      int centenas;
+      int decenas;
+      int unidades= decenas/10;
+      return romanic;  
+    }
+    
+}
--- Ex9_Erik_Isaac/src/ex9_erik_isaac/Ex9_Erik_Isaac.java
+++ Ex9_Erik_Isaac/src/ex9_erik_isaac/Ex9_Erik_Isaac.java
@@ -5,16 +5,54 @@
  */
 package ex9_erik_isaac;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author alumne
  */
 public class Ex9_Erik_Isaac {
    
-        
+            
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        int number = 0;
+        number = inputPossitiveNumber();
+        
+        //System.out.println(number);
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    public static int inputPossitiveNumber() {
+        int number = 0; 
+        boolean correct = false;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        do {
+            try {
+                System.out.print("Insert number:");
+                number = Integer.parseInt(reader.readLine());
+                if(number > 0) {
+                    correct = true;
+                } else {
+                    System.out.println("Wrong value, expected possitive number.");
+                }
+            } catch (IOException e) {
+                System.out.println("Unexpected error.");
+            } catch (NumberFormatException e) {
+                System.out.println("Wrong value, expected a number.");
+            }
+        } while(!correct);
+        return number;
     }
     
 }
